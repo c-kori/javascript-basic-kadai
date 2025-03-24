abaa25d88ef213ab5acdf0cfa37fa776a6384823
c-kori <io79135tisa@gmail.com>
2025-03-25 00:07:15 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,21 @@
+// 変数numに0～2までのランダムな整数を代入する
+
+let num = 4;
+
+if (num %3==0 && num %5==0) {
+  console.log('3と5の倍数です');
+}
+
+else if (num %5==0) {
+  console.log('5の倍数です');
+}
+
+
+else if (num %3==0) {
+  console.log('3の倍数です');
+}
+
+
+else {
+  console.log(num);
+}
