62fdd768e2fe390be53b46886e9575d5b9575d9d
c-kori <io79135tisa@gmail.com>
2025-03-26 00:20:38 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const moonLanding = new Date()
+
+const Year =moonLanding.getFullYear()
+const Month = moonLanding.getMonth()
+const Day = moonLanding.getDate()
+
+console.log(Year+'年'+(Month+1)+'月'+Day+'日');
